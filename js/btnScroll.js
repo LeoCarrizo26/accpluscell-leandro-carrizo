d0f706c0869438dcc6fa8ee004b74bbddb324896
LeoCarrizo26 <leocarrizo_26@hotmail.com>
2022-08-06 11:00:24 -0300

    Botón de scroll(continuación)

--- js/btnScroll.js
+++ js/btnScroll.js
@@ -1,13 +1,17 @@
-$('.btn-scroll').html  (function up (px){
-    window.addEventListener('scroll', ()=>{
-        var scroll = document.documentElement.scrollTop;
-        var btn-scroll = document.getElementById('btn-scroll');
-        if (scroll > px){
-            btn-scroll.style.left == 20 + "px";
-        }    else {
-                    btn-scroll.style.left == 100 + "px";
-        }
-    })
-    up (500);
+$(document).ready(function(){
 
-})
+	$('.btn-scroll').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 500 ){
+			$('.btn-scroll').slideDown(1000);
+		} else {
+			$('.btn-scroll').slideUp(1000);
+		}
+	});
+
+});
